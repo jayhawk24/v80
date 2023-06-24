59cce79eac587d5cce6f992cd5f8e714ad2a6d2e
anubhav <anubhavthrilling24@gmail.com>
2023-06-24 15:36:09 +0530

    fix(utils): fix formatData

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -17,15 +17,9 @@ declare module "next-auth" {
   interface Session extends DefaultSession {
     user: {
       id: string;
-      // ...other properties
-      // role: UserRole;
     } & DefaultSession["user"];
   }
 
-  // interface User {
-  //   // ...other properties
-  //   // role: UserRole;
-  // }
 }
 
 /**
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,24 +1,25 @@
-import { type Series, type SeriesData, type TimeSeriesDailyAdjusted } from "~/types/types";
+import {
+  type Series,
+  type SeriesData,
+  type TimeSeriesDailyAdjusted,
+} from "~/types/types";
 
-export const formatData= (stockData: TimeSeriesDailyAdjusted) : Series => {
-  const solution : SeriesData[] = [];
+export const formatData = (stockData: TimeSeriesDailyAdjusted): Series => {
+  const solution: SeriesData[] = [];
 
   Object.entries(stockData["Time Series (Daily)"]).forEach(
-  ([month, monthlyPrices]) => {
-    const obj : SeriesData = {};
-    const prices = [];
+    ([month, monthlyPrices]) => {
+      const obj: SeriesData = { x: new Date(month), y: [] };
+      const prices = [];
 
-    obj.x = new Date(month);
+      prices.push(Number(monthlyPrices["1. open"]));
+      prices.push(Number(monthlyPrices["2. high"]));
+      prices.push(Number(monthlyPrices["3. low"]));
+      prices.push(Number(monthlyPrices["4. close"]));
+      obj.y = prices;
 
-    prices.push(Number(monthlyPrices["1. open"]))
-    prices.push(Number(monthlyPrices["2. high"]))
-    prices.push(Number(monthlyPrices["3. low"]))
-    prices.push(Number(monthlyPrices["4. close"]))
-    obj.y = prices;
-
-    solution.push(obj);
-  },
-
+      solution.push(obj);
+    }
   );
-  return {data : solution.sort((a, b) => a.x - b.x) };
-  }
+  return { data: solution };
+};
